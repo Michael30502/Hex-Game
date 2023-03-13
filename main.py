fee338114f2970d30263d06a098dd98f9a12e3c4
TroelsLucas <96730274+TroelsLucas@users.noreply.github.com>
2023-03-13 12:01:52 +0100

    1

--- main.py
+++ main.py
@@ -7,20 +7,42 @@ RED = (255, 0, 0)
 
 FPS = 60
 
-# Surface/Screen size (this shoudl be scaleable)
+run = True
+first_menu = True
+setting_menu = False
+second_menu = False
+game_running = False
+game_paused = False
+game_finished = False
 
-WIDTH, HEIGHT = 900, 500
+# Surface/Screen size (this shoudl be scaleable
+WIDTH, HEIGHT = 800, 800
 game_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("HEX")
 
-start_button_img =  pygame.image.load("Hex-Game/assets/start_button_img.png").convert_alpha()
-hexagon_neutral_img = pygame.image.load("Hex-Game/assets/tile_0.png").convert_alpha()
-hexagon_player1_img = pygame.image.load("Hex-Game/assets/tile_1.png").convert_alpha()
-hexagon_player2_img = pygame.image.load("Hex-Game/assets/tile_2.png").convert_alpha()
+
+#load button images
+start_game_img = pygame.image.load('assets/Start_Game.png').convert_alpha()
+AI_1_img = pygame.image.load('assets/AI_1.png').convert_alpha()
+Play_Online_img = pygame.image.load('assets/Play_Online.png').convert_alpha()
+AI_2_img = pygame.image.load('assets/AI_2.png').convert_alpha()
+AI_3_img = pygame.image.load('assets/AI_3.png').convert_alpha()
+Change_Board_Size_img = pygame.image.load('assets/Change_Board_Size.png').convert_alpha()
+Continue_img = pygame.image.load('assets/Continue.png').convert_alpha()
+Export_Game_img = pygame.image.load('assets/Export_Game.png').convert_alpha()
+Import_Game_img = pygame.image.load('assets/Import_Game.png').convert_alpha()
+Pause_Game_img = pygame.image.load('assets/Pause_Game.png').convert_alpha()
+Settings_img = pygame.image.load('assets/Settings.png').convert_alpha()
+Two_Player_img = pygame.image.load('assets/Two_Player.png').convert_alpha()
+Go_Back_img = pygame.image.load('assets/Go_Back.png').convert_alpha()
+
+start_button_img =  pygame.image.load("assets/start_button_img.png").convert_alpha()
+hexagon_neutral_img = pygame.image.load("assets/tile_0.png").convert_alpha()
+hexagon_player1_img = pygame.image.load("assets/tile_1.png").convert_alpha()
+hexagon_player2_img = pygame.image.load("assets/tile_2.png").convert_alpha()
 
 # game variables
 action= False
-
 
 class Button:
     def __init__(self, x, y, image, scale, unit):
@@ -166,9 +188,6 @@ class EventHandler:
     def __init__(self, event):
         self.event = event
 
-
-
-
 class Game:
     def __init__(self, surface, board, turn):
         self.surface = surface
@@ -176,6 +195,7 @@ class Game:
         self.board = board
         self.running = True
         self.game_menu = False
+        self.run = True
 
     def menu(self):
         height = game_surface.get_height()
@@ -192,6 +212,7 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.game_menu = False
                     self.running = False
+                    self.run = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         game_surface.fill(WHITE)
@@ -234,19 +255,108 @@ class Game:
     def turn_count(self):
         self.turn += 1
 
+class MenuButton():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
 
-# button inits
+	def drawMenu(self, surface):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
+
+#create button instances
+start_game_button = MenuButton(0, 0, start_game_img, 2)
+play_online_button = MenuButton(300, 200, Play_Online_img, 2)
+ai_2_button = MenuButton(300, 400, AI_2_img, 2)
+two_player_button = MenuButton(300, 100, Two_Player_img, 2)
+ai_1_button = MenuButton(300, 300, AI_1_img, 2)
+ai_3_button = MenuButton(300, 500, AI_3_img, 2)
+change_board_size_button = MenuButton(300, 200, Change_Board_Size_img, 2)
+settings_button = MenuButton(100, 200, Settings_img, 2)
+continue_button = MenuButton(300, 200, Continue_img, 2)
+export_game_button = MenuButton(300, 100, Export_Game_img, 2)
+import_game_button = MenuButton(300, 100, Import_Game_img, 2)
+pause_game_button = MenuButton(600, 600, Pause_Game_img, 1)
+go_back_button = MenuButton(600, 600, Go_Back_img, 1)
 hexagon1 = Button(0, 0, hexagon_neutral_img, 0.5, (0, 0))
 hexagon2 = Button(hexagon_neutral_img.get_width() * 0.5, 0, hexagon_neutral_img, 0.5, (0, 1))
-# hexagon_neutral_img = pygame.image.load("Hex-Game/assets/tile_0.png").convert_alpha()
 hexagon_neutral_img_mask = pygame.mask.from_surface(hexagon_neutral_img)
-# hexagon3 = Button(0,0, hexagon_neutral_img_mask,0.5, (0,0))
-
 hexagon_player1 = Button(96, 0, hexagon_player1_img, 0.5, (1, 1))
 
-board1 = Board(hexagon1, gamelogic.board_size, game_surface)
-game1 = Game(game_surface, board1, 0)
-game1.play()
+
+
+# game loop
+
+while run:
+    game_surface.fill((200, 200, 255))
+    hexagon1.draw(game_surface)
+
+    if first_menu == True:
+        if start_game_button.drawMenu(game_surface):
+            print('first menu')
+            second_menu = True
+            first_menu = False
+        if settings_button.drawMenu(game_surface):
+            first_menu = False
+            setting_menu = True
+            
+    if second_menu == True:
+        if two_player_button.drawMenu(game_surface):
+                print('2 player')
+                game_running = True
+                second_menu = False	
+        if ai_1_button.drawMenu(game_surface):
+                print('playing against bot 1')
+        if ai_2_button.drawMenu(game_surface):
+                print('playing against bot 1')
+        if ai_3_button.drawMenu(game_surface):
+                print('playing against bot 1')
+        if play_online_button.drawMenu(game_surface):
+                print('playing against bot 1')
+        if go_back_button.drawMenu(game_surface):
+                first_menu = True
+                second_menu = False
+
+    if game_running == True:
+        board1 = Board(hexagon1, gamelogic.board_size, game_surface)
+        game1 = Game(game_surface, board1, 0)
+        game1.play()
+
+    if setting_menu == True:
+            if change_board_size_button.drawMenu(game_surface):
+                print('change board size')
+            if import_game_button.drawMenu(game_surface):
+                print('import saved game')
+            if go_back_button.drawMenu(game_surface):
+                setting_menu = False
+                first_menu = True  
+
+    for event in pygame.event.get():
+		    #quit game
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.display.update()
+
+pygame.quit()
 
 # running = True
 # while running:
